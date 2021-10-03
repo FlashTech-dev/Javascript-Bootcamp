d818583b504c5665224c0dc0bf87627e7e98e4d6
Karan <karanwagh17@gmail.com>
2021-10-03 14:14:04 +0530

    strict mode

---
+++ Fundamentals/strictmode/script.js
@@ -0,0 +1,18 @@
+//try running the first multiline comment first by removing the /* -- */ and check the console.log you will see no errors//
+
+/* //'use strict';//
+let hasAge= false;
+const vote =true;
+if(vote) hasAg= true;
+if(hasAge) console.log('you can vote');
+*/
+
+// now try running this//
+/* 'use strict';
+let hasAge= false;
+const vote =true;
+if(vote) hasAg= true;  //wrong variable name wrote on purpose 
+if(hasAge) console.log('you can vote');
+*/
+
+// you must have seen the error thrown in the console after you open the index.html file//
